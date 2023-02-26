953354b1913289a1d41029ce0ea37e8ae8d709cd
sergkos <kostrovs@gmail.com>
2023-02-26 09:48:13 +0300

    some add

--- seminar_1/Program.java
+++ seminar_1/Program.java
@@ -1,63 +1,65 @@
 //1. Вычислить n-ое треугольное число (сумма чисел от 1 до n), n!(произведение чисел от 1 до n).
-//import java.util.Scanner;
-//public class Program {
-//     public static void main(String[] args) {
-//
-//         try {
-//             int number = getInt("Enter number");
-//             int factorial = factor(number);
-//             int triangle = triangle(number);
-//             System.out.printf("---------\nFactorial %d = %d\n", number, factorial);
-//             System.out.printf("---------\nTriangle %d = %d\n", number, triangle);
-//         }
-//         catch (Exception e){
-//             System.out.println("quit.");
-//         }
-//     }
-//
-//    private static int triangle(int n) {
-//         return n*(n+1)/2;
-//    }
-//
-//    static int getInt(String text) {
-//         Scanner in = new Scanner(System.in);
-//         System.out.println(text);
-//         Integer num = null;
-//         try {
-//             num = in.nextInt();
-//         }
-//         catch (Exception e) {
-//             System.out.println("Input error");
-//         }
-//         in.close();
-//         return num;
-//     }
-//     static int factor(int x) {
-//         int factor_ = 1;
-//         for (int i = 1; i <= x; i++) {
-//             factor_ = factor_ * i;
-//         }
-//         return factor_;
-//     }
-//}
 
+import java.util.Scanner;
+public class Program {
+    public static void main(String[] args) {
+
+        try {
+            int number = getInt("Enter number");
+            int factorial = factor(number);
+            int triangle = triangle(number);
+            System.out.printf("---------\nFactorial %d = %d\n", number, factorial);
+            System.out.printf("---------\nTriangle %d = %d\n", number, triangle);
+        }
+        catch (Exception e){
+            System.out.println("quit.");
+        }
+    }
+
+   private static int triangle(int n) {
+        return n*(n+1)/2;
+   }
+
+   static int getInt(String text) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(text);
+        Integer num = null;
+        try {
+            num = in.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("Input error");
+        }
+        in.close();
+        return num;
+    }
+    static int factor(int x) {
+        int factor_ = 1;
+        for (int i = 1; i <= x; i++) {
+            factor_ = factor_ * i;
+        }
+        return factor_;
+    }
+}
 
 //2. Вывести все простые числа от 1 до 1000.
-// public class Program {
-//    public static void main(String[] args) {
-//        int max = 100;
-//        for (int i = 1; i <= max; i++) {
-//            for (int j = 2; j < i; j++) {
-//                if (i % j == 0) break;
-//                if (!((j+1) < i)) System.out.printf("%d ",i);
-//            }
-//        }
-//    }
-// }
+
+public class Program {
+   public static void main(String[] args) {
+       int max = 100;
+       for (int i = 1; i <= max; i++) {
+           for (int j = 2; j < i; j++) {
+               if (i % j == 0) break;
+               if (!((j+1) < i)) System.out.printf("%d ",i);
+           }
+       }
+   }
+}
 
 //3. Реализовать простой калькулятор.
- import java.util.Scanner;
- public class Program {
+
+import java.util.Scanner;
+public class Program {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Enter a: ");
@@ -70,20 +72,21 @@
  }
 
 //4. Уравнение вида q + w = e; q,w,e >= 0; 2? + ?5 = 69. Предложить решение.
-// public class Program {
-//    public static void main(String[] args) {
-//        String q = "2x";
-//        String w = "y5";
-//        int e = 69;
 
-//        for (int x = 0; x <= 9; x++) {
-//            int str_q = Integer.parseInt(q.replace("x", Integer.toString(x)));
-//            for (int y = 0; y <= 9; y++) {
-//                int str_w = Integer.parseInt(w.replace("y", Integer.toString(y)));
-//                if (str_q + str_w == e){
-//                    System.out.printf("Formula 2? + ?5 = 69 result: first'?' = %d, second'?' = %d\n", x, y);
-//                }
-//            }
-//        }
-//    }
-// }
+public class Program {
+   public static void main(String[] args) {
+       String q = "2x";
+       String w = "y5";
+       int e = 69;
+
+       for (int x = 0; x <= 9; x++) {
+           int str_q = Integer.parseInt(q.replace("x", Integer.toString(x)));
+           for (int y = 0; y <= 9; y++) {
+               int str_w = Integer.parseInt(w.replace("y", Integer.toString(y)));
+               if (str_q + str_w == e){
+                   System.out.printf("Formula 2? + ?5 = 69 result: first'?' = %d, second'?' = %d\n", x, y);
+               }
+           }
+       }
+   }
+}
--- seminar_5/task_1.java
+++ seminar_5/task_1.java
@@ -2,15 +2,42 @@
 Реализуйте структуру телефонной книги с помощью HashMap,
 учитывая, что 1 человек может иметь несколько телефонов.
 */
+import java.util.Scanner;
 
 public class main {
 
-   public static void main(String[] args) {
-    Map <Integer, List<String>> map = new HashMap<>();
-    
+	public static void main(String[] args) {
+		Map<String, List<String>> map = new HashMap<>();
+		Scanner iScanner = new Scanner(System.in);
 
-   }
-   static void addNumber(map){
+		addNumber();
+		// print();
 
-   }
+		iScanner.close();
+
+	}
+
+	static void addNumber(){
+
+		System.out.println("Enter human name: ");
+		String name = iScanner.next();
+		System.out.println("Enter phone number: ");
+		String phone = iScanner.next();
+
+		if (map.containskey(name)){
+			List<String> phones = map.get(name);
+			phones.add(phone);
+		}
+		else{
+			List<String> phones = new ArrayList<>();
+			phones.add(phone);
+			map.put(name, phones);
+		}
+	}
+
+	// static void print(){
+	// 	for (Map.Entry<String, List<String>> entry: map.entrySet()) {
+	// 		System.out.println(entry.getKey() + ":" + entry.getValue());
+	// 	}
+	// }
 }
