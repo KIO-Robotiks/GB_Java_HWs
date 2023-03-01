c2ef88d5f666ef8b0d926352593d88d2151c752f
sergkos <kostrovs@gmail.com>
2023-03-01 11:31:22 +0300

    DZ №5

--- seminar_5/task_1.java
+++ seminar_5/task_1.java
@@ -2,29 +2,39 @@
 Реализуйте структуру телефонной книги с помощью HashMap,
 учитывая, что 1 человек может иметь несколько телефонов.
 */
-import java.util.Scanner;
+import java.util.*;
 
-public class main {
+import static java.lang.System.exit;
+
+public class task_1 {
 
 	public static void main(String[] args) {
 		Map<String, List<String>> map = new HashMap<>();
 		Scanner iScanner = new Scanner(System.in);
-
-		addNumber();
-		// print();
-
-		iScanner.close();
-
+		while (true){
+			System.out.println("--------------------");
+			System.out.println("Currently phonebook:");
+			printKeys(map);
+			System.out.println("--------------------");
+			System.out.println("If you add number enter 'add'");
+			System.out.println("If you print all numbers enter 'print'");
+			System.out.println("For exit enter 'exit'");
+			String choose = iScanner.next();
+			System.out.println(choose);
+			if (choose.equals("add")) addNumber(map, iScanner);
+			else if	(choose.equals("print"))	print(map);
+			else if	(choose.equals("exit"))	exit(0);
+		}
 	}
 
-	static void addNumber(){
+	static void addNumber(Map<String, List<String>> map, Scanner iScanner){
 
 		System.out.println("Enter human name: ");
 		String name = iScanner.next();
 		System.out.println("Enter phone number: ");
 		String phone = iScanner.next();
 
-		if (map.containskey(name)){
+		if (map.containsKey(name)){
 			List<String> phones = map.get(name);
 			phones.add(phone);
 		}
@@ -35,9 +45,16 @@ public class main {
 		}
 	}
 
-	// static void print(){
-	// 	for (Map.Entry<String, List<String>> entry: map.entrySet()) {
-	// 		System.out.println(entry.getKey() + ":" + entry.getValue());
-	// 	}
-	// }
+	 static void print(Map<String, List<String>> map){
+	 	for (Map.Entry<String, List<String>> entry: map.entrySet()) {
+	 		System.out.println(entry.getKey() + ":" + entry.getValue());
+	 	}
+	 }
+	static void printKeys(Map<String, List<String>> map){
+		for (Map.Entry<String, List<String>> entry: map.entrySet()) {
+			System.out.printf(entry.getKey()+ " ");
+
+		}
+		System.out.println();
+	}
 }
--- seminar_5/task_2.java
+++ seminar_5/task_2.java
@@ -5,8 +5,46 @@
 Отсортировать по убыванию популярности. Для сортировки использовать TreeMap.
 */
 
-public class main {
+import java.util.*;
+
+public class task_2 {
 
    public static void main(String[] args) {
+      String strInput = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов";
+      String[] arrInput = strInput.split(", ");
+
+      TreeMap<String, Integer> map = new TreeMap<>();
+
+      String name;
+      for (String str: arrInput) {
+         name = str.split(" ")[0];
+         if (map.containsKey(name)){
+            Integer value = map.get(name);
+            value ++;
+            map.put(name, value);
+         }
+         else map.put(name, 1);
+      }
+      print(getSortedMap(map));
+   }
+
+   static void print(TreeMap<String, Integer> map) {
+      for (String key : map.keySet()) {
+         String value = map.get(key).toString();
+         System.out.println(key + " " + value);
+      }
+   }
+   static TreeMap<String, Integer> getSortedMap(TreeMap<String, Integer> map) {
+      Comparator<String> valueComparator = new Comparator<String>() {
+         @Override
+         public int compare(String a, String b) {
+            Integer v1 = map.get(a);
+            Integer v2 = map.get(b);
+            return v2.compareTo(v1);
+         }
+      };
+      TreeMap<String, Integer> sortedMap = new TreeMap<>(valueComparator);
+      sortedMap.putAll(map);
+      return sortedMap;
    }
 }
